a37ff70717efe609b555fbf25767489a6f3c5a2f
Samantha Fok <samantha.fok@hotmail.com>
2020-11-19 21:32:55 +0000

    Escaped user text so that it will be protected for XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,39 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+//<script>alert("Hey")</script>
 $(document).ready(function() {
-  
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
   
   const renderTweet = function(data) {
     let $userTweet = "";
-    $(".tweet-container").empty();
+    //$(".tweet-container").empty();
     for (const tweet of data) {
       $userTweet = createTweetElement(tweet);
+      // $(".tweet-container").text($userTweet)
       $(".tweet-container").prepend($userTweet);
     }
   };
@@ -43,7 +19,15 @@ $(document).ready(function() {
   const createTweetElement = function(data) {
     let date = new Date(data["created_at"]);
     date = date.toDateString();
-    const $tweet = $(`        <article class="tweet">
+
+    const escape =  function(str) {
+      let div = document.createElement('div');
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    }
+    const safeHTML = `<p>${escape(data["content"]["text"])}</p>`;
+
+    const $tweet = $(`<article class="tweet">
     <header>
       <div>
       <img src=${data["user"]["avatars"]}>
@@ -51,26 +35,27 @@ $(document).ready(function() {
       <p class="user">${data["user"]["handle"]}</p>
     </header>
     <section class="past-posts">
-      ${data["content"]["text"]}
+      ${safeHTML}
     </section>
     <hr/>
     <footer class="tweet">
       <p class="data-stamp">${date}</p>
       <p class="share-links">share links</p>
     </footer>
-  </article>`);
+    </article>`);
+    // $("<section>").text(data["content"]["text"]);
     return $tweet;
   };
 
   $("#button").on("click", function(event) {
     event.preventDefault();
     let newtweets = $("#tweet-text").serialize();
-    console.log(newtweets.length)
-    if (newtweets.length > 203) { //including the serials
+    $("<form>").text(newtweets)///////
+    if (newtweets.length > 203) {
       alert("Your tweet has exceeded the max character count! Please revise your post.");
       return;
     }
-    if (newtweets.length === 5) { //text=
+    if (newtweets.length === 5) {
       alert("The input field is empty! Please write a message.");
       return;
     }
@@ -85,7 +70,6 @@ $(document).ready(function() {
     $.get("/tweets")//array of json tweets
     .then(function(data) {
       return renderTweet(data)
-      //console.log(data)
     })
   };
   loadTweets();
